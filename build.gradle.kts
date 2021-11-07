b95c6272e0405f1bd8ec275d40be6091a4e33c5d
huixing <wanghuixing24@gmail.com>
2021-11-07 11:29:57 +0800

    support multiple platforms

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
+    }
+}
+
+group = "com.godaddy"
+version = "1.0"
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
---
+++ color-picker/src/androidMain/kotlin/me/lierxiao/common/platform.kt
@@ -0,0 +1,5 @@
+package me.lierxiao.common
+
+//actual fun getPlatformName(): String {
+//    return "Android"
+//}
